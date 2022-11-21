ba206972a2f5e4eec489d316a69b8c211f5960c3
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-21 15:57:52 +0300

    Completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,22 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    # render json: cheeses, only: [:id, :name]
+    # serialization
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
+
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese
+    else
+      render json: { error: "Cheese not found"}, status: 404
+      # status: :not_found
+      end
+    # render json: cheese
   end
 
 end
